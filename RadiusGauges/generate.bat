281885b775dff8355838d216e88f3197c1c27b9a
clough <james.clough@hp.com>
2016-06-20 13:52:08 -0600

    Rewrite radius gauge generation script.  Organize radius gauges into sets.

--- RadiusGauges/generate.bat
+++ RadiusGauges/generate.bat
@@ -2,112 +2,20 @@
 
 set OPENSCAD="c:\Program Files\OpenSCAD\openscad.exe"
 
-for %%x IN (
-	1,
-	3,
-	5,
-	7,
-	9,
-	11,
-	13,
-	15,
-	17,
-	19,
-	21,
-	23,
-	25,
-	27,
-	29,
-	31
-) DO %OPENSCAD% -o STL\Imperial\RadiusGauge-%%x-32-in.stl -Dnumber=%%x -Dscale=25.4/32 -Dunit=\"/32in\" RadiusGauge.scad
 
-for %%x IN (
-	1,
-	3,
-	5,
-	7,
-	9,
-	11,
-	13,
-	15
-) DO %OPENSCAD% -o STL\Imperial\RadiusGauge-%%x-16-in.stl -Dnumber=%%x -Dscale=25.4/16 -Dunit=\"/16in\" RadiusGauge.scad
-
-for %%x IN (
-	1,
-	3,
-	5,
-	7,
-) DO %OPENSCAD% -o STL\Imperial\RadiusGauge-%%x-8-in.stl -Dnumber=%%x -Dscale=25.4/8 -Dunit=\"/8in\" RadiusGauge.scad
-
-for %%x IN (
-	1,
-	3
-) DO %OPENSCAD% -o STL\Imperial\RadiusGauge-%%x-4-in.stl -Dnumber=%%x -Dscale=25.4/4 -Dunit=\"/4in\" RadiusGauge.scad
-
-for %%x IN (
-	1
-) DO %OPENSCAD% -o STL\Imperial\RadiusGauge-%%x-2-in.stl -Dnumber=%%x -Dscale=25.4/2 -Dunit=\"/2in\" RadiusGauge.scad
-
-for %%x IN (
-	1
-) DO %OPENSCAD% -o STL\Imperial\RadiusGauge-%%x-in.stl -Dnumber=%%x -Dscale=25.4 -Dunit=\"in\" RadiusGauge.scad
-
-for %%x IN (
-	1,
-	1.5,
-	2,
-	2.5,
-	3,
-	3.5,
-	4,
-	4.5,
-	5,
-	5.5,
-	6,
-	6.5,
-	7,
-	7.5,
-	8,
-	8.5,
-	9,
-	9.5,
-	10,
-	10.5,
-	11,
-	11.5,
-	12,
-	12.5,
-	13,
-	13.5,
-	14,
-	14.5,
-	15,
-	15.5,
-	16,
-	16.5,
-	17,
-	17.5,
-	18,
-	18.5,
-	19,
-	19.5,
-	20,
-	20.5,
-	21,
-	21.5,
-	22,
-	22.5,
-	23,
-	23.5,
-	24,
-	24.5,
-	25
-) DO %OPENSCAD% -o STL\Metric\RadiusGauge-%%x-mm.stl -Dnumber=%%x -Dscale=1 -Dunit=\"mm\" RadiusGauge.scad
+mkdir STL\Imperial\Small
+mkdir STL\Imperial\Large
+mkdir STL\Metric\Small
+mkdir STL\Metric\Large
 
 
 
 
+FOR /F "tokens=1-3" %%x IN (imperialSizes.txt) DO %OPENSCAD% -o STL\Imperial\%%z\RadiusGauge-%%x-%%y-in.stl -Dnumber=%%x -Dscale=25.4/%%y -Dunit=\"/%%yin\" RadiusGauge.scad
+
+%OPENSCAD% -o STL\Imperial\Large\RadiusGauge-1-in.stl -Dnumber=1 -Dscale=25.4 -Dunit=\"in\" RadiusGauge.scad
 
 
 
-	
+
+FOR /F "tokens=1-2" %%x IN (metricSizes.txt) DO %OPENSCAD% -o STL\Metric\%%y\RadiusGauge-%%x-mm.stl -Dnumber=%%x -Dscale=1 -Dunit=\"mm\" RadiusGauge.scad
